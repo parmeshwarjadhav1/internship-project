af5e2cbdb84d9e4e1b2f4db927b0a7a70aabb84b
Umesh Rohidas Jadhav <umeshjadhavxig@gmail.com>
2024-07-18 15:28:30 +0530

    UseGitHub File Thats Print Method Changes

---
+++ UseGitHub.java
@@ -0,0 +1,12 @@
+package InternshipProjects;
+
+public class UseGitHub {
+   public static void main(String arg[])
+   {
+	   int a = 5;
+	   int b = 5;
+	    int c = a+b;
+	  // System.out.println("Addition of two nubers :- "+(a+b));
+     System.out.println(c+" ");
+   }
+}
